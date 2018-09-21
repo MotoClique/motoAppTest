6a5a33e68853ef99af7612ac0897952f2dfcc018
MotoClique <40442195+MotoClique@users.noreply.github.com>
2018-09-21 18:19:16 +0530

    mlab url switch

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+//Switch flag for Production DB / Testing DB
+var prodEnvFlag = false;
+
 //  Moto Clique Node application
 //importing modules
 
@@ -103,8 +106,11 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 var db = null,
     dbDetails = new Object();
 	
-var mongoURL = "mongodb://meanadmin:Moto1234@ds235302.mlab.com:35302/meandb",
- mongoURLLabel = "mongodb://meanadmin:Moto1234@ds235302.mlab.com:35302/meandb";
+var prdDBUrl = "mongodb://motoadmin:Moto1234@ds217002.mlab.com:17002/motodb";
+var testDBUrl = "mongodb://meanadmin:Moto1234@ds235302.mlab.com:35302/meandb";
+var mongoURL = (prodEnvFlag)?prdDBUrl:testDBUrl ,
+ mongoURLLabel = (prodEnvFlag)?prdDBUrl:testDBUrl;
+
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
